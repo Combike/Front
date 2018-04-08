96478307e0dc984868c691cd874171bf0a02d4aa
Carlos Silva <carlos.silva@gsd.com.br>
2018-04-08 05:16:57 -0300

    New route form

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Combike</h1>
     <button @click="">Usar GPS</button>
+    <router-link tag='button' :to="{name: 'AddRoute'}">Cadastrar rota</router-link>
     <div class="google-map" :id="mapName"></div>
   </div>
 </template>
@@ -16,37 +17,41 @@ export default {
     }
   },
   mounted: function () {
+    // Sorry
+    setTimeout(() => {
+      const element = document.getElementById(this.mapName)
+      const options = {
+        zoom: 14,
+        center: new google.maps.LatLng(-3.7394102,-38.5271261)
+      }
+      const map = new google.maps.Map(element, options)
+
+      let geocoder = new google.maps.Geocoder()
+      let directionsService = new google.maps.DirectionsService()
+
+      const routes = [
+        [{lat: -3.7394102, lng: -38.5271261}, {lat: -3.7413959, lng: -38.5434447}],
+        [{lat: -3.747991, lng: -38.5355487}, {lat: -3.7513309, lng: -38.5200137}],
+      ]
+      routes.forEach(route => {
+        let request = {
+          origin: route[0],
+          destination: route[1],
+          travelMode: google.maps.DirectionsTravelMode.BICYCLING
+        }
+        directionsService.route(request, (response, status) => {
+          if (status == google.maps.DirectionsStatus.OK) {
+            const directionsRenderer = new google.maps.DirectionsRenderer()
+            directionsRenderer.setDirections(response)
+            directionsRenderer.setMap(map)
+          }
+        })
+      })
+    }, 500)
     // const input = document.getElementById('route_from')
     // const autocomplete = new google.maps.places.Autocomplete(input)
-    const element = document.getElementById(this.mapName)
-    const options = {
-      zoom: 14,
-      center: new google.maps.LatLng(-3.7394102,-38.5271261)
-    }
-    const map = new google.maps.Map(element, options)
-
-    let geocoder = new google.maps.Geocoder()
-    let directionsService = new google.maps.DirectionsService()
-
-    const routes = [
-      [{lat: -3.7394102, lng: -38.5271261}, {lat: -3.7413959, lng: -38.5434447}],
-      [{lat: -3.747991, lng: -38.5355487}, {lat: -3.7513309, lng: -38.5200137}],
-    ]
-    routes.forEach(route => {
-      let request = {
-        origin: route[0],
-        destination: route[1],
-        travelMode: google.maps.DirectionsTravelMode.BICYCLING
-      }
-      directionsService.route(request, (response, status) => {
-        if (status == google.maps.DirectionsStatus.OK) {
-          const directionsRenderer = new google.maps.DirectionsRenderer()
-          directionsRenderer.setDirections(response)
-          directionsRenderer.setMap(map)
-        }
-      })
-    })
-  }
+  },
+  methods: {}
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
+import AddRoute from '@/components/AddRoute'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/add-route',
+      name: 'AddRoute',
+      component: AddRoute
     }
   ]
 })

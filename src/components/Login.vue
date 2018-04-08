085876943d1c0ffb310d03073b78f4d1e1dbc704
Maicol Santos <maicolcostaa8@gmail.com>
2018-04-08 06:00:19 -0300

    Página home com vuetify

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,7 +5,7 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
+              <v-toolbar dark color="orange">
                 <v-toolbar-title>Login form</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
@@ -16,7 +16,7 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" @click="login">Login</v-btn>
+                <v-btn color="orange" @click="login">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
